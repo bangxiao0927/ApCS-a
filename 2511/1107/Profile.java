172a6204fd8a79c71320a2a8b204ba580738c87d
Bang Xiao <bangxiao0927@gmail.com>
2025-11-11 10:33:18 -0800

    Finished 1107

--- 2511/1107/Profile.java
+++ 2511/1107/Profile.java
@@ -1,3 +1,11 @@
 public class Profile {
-    
+    private final String name; //final no change after initialized
+
+    public Profile(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
 }
--- 2511/1107/Runner.java
+++ 2511/1107/Runner.java
@@ -1,3 +1,9 @@
 public class Runner {
-    
+    public static void main(String[] args) {
+        ArrayTest arrayTest = new ArrayTest();
+        arrayTest.arrayDemo1();
+        arrayTest.arrayDemo2();
+        arrayTest.arrayDemo3();
+        arrayTest.arrayDemo4();
+    }
 }
