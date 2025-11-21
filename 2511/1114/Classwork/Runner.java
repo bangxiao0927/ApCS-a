7cf5a3f66217310d4f7b79e0035dece9ea5c09d8
Bang Xiao <bangxiao0927@gmail.com>
2025-11-20 20:04:45 -0800

    Added runner

--- 2511/1114/Classwork/Runner.java
+++ 2511/1114/Classwork/Runner.java
@@ -1,3 +1,12 @@
+import javax.swing.JFrame;
+
 public class Runner {
-    
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Tree Screen");
+        Screen screen = new Screen();
+        frame.add(screen);
+        frame.setSize(800, 600);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
 }
