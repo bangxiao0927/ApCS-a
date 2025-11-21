df1564a92cb40f8721e2001c14a5a54ba1cd920d
Bang Xiao <bangxiao0927@gmail.com>
2025-11-21 13:45:04 -0800

    Initialized 1121

---
+++ 2511/1121/Classwork/Player.java
@@ -0,0 +1,61 @@
+import java.awt.Color;
+import java.awt.Graphics;
+
+
+public class Player{
+	private int x;
+	private int y;
+	
+	private int width;
+	private int height;
+	
+	private Color blue;
+	
+	public Player(int x, int y){
+		this.x = x;
+		this.y = y;
+		this.width = 50;
+		this.height = 50;
+		this.blue = new Color(0,0,255);
+	}
+	
+	public void drawMe(Graphics g){
+		g.setColor(blue);
+		g.fillRect(x,y,width,height);
+	}
+	
+	public int getX(){
+		return x;
+	}
+	
+	public int getY(){
+		return y;
+	}
+	
+	public int getWidth(){
+		return width;
+	}
+	
+	public int getHeight(){
+		return height;
+	}
+	
+
+
+	public void moveUp(){
+		y = y - 5;
+	}
+	
+	public void moveDown(){
+		y = y + 5;
+	}
+	
+	public void moveLeft(){
+		x = x - 5;
+	}
+	
+	public void moveRight(){
+		x = x + 5;
+	}
+	
+}
