1c9b4e355179b94187ca3fa4b9edac9140d5a220
Bang Xiao <bangxiao0927@gmail.com>
2026-03-16 19:11:33 -0700

    Finished 0316

---
+++ 2603/0316/warmup/WarmUp.java
@@ -0,0 +1,31 @@
+public class WarmUp {
+    public int[][] create2DArray() {
+        int[][] array = new int[4][5];
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                array[i][j] = (int) (Math.random() * 5) + 1;
+            }
+        }
+        return array;
+    }
+
+    public void print(int[][] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                System.out.print(array[i][j] + "\t");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    public void replace(int[][] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                if (array[i][j] == 1) {
+                    array[i][j] = 0;
+                }
+            }
+        }
+    }
+}
